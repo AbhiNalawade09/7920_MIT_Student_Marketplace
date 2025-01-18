0b561f05111a60070aa49f54734f0e34584058e0
Rajan gupta <81641542+Rajangupta9@users.noreply.github.com>
2025-01-18 18:19:18 +0530

    add cors

--- backend/app.js
+++ backend/app.js
@@ -14,16 +14,22 @@ const app = express();
 connectDB();
 
 app.use(express.json());
+app.use(cors());
 
 // Routes
 app.get('/', (req, res) => {
     res.send('API is running...');
 });
+app.get('')
 app.use('/api/users', userRoutes);
 app.use('/api/products', productRoutes);
 app.use('/api/orders', orderRoutes);
 app.use('/api/reviews', reviewRoutes);
 app.use('/api/categories', categoryRoutes);
+
+app.use( (req, res, next) => {
+    res.status(404).json({ message: "this api is not defined" });
+});
 
 const PORT = process.env.PORT || 5000;
 
